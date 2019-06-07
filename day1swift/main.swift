80b8ff165715464b38ef35dc00e8a0619c7e7908
Ujwal <ujwalarora.nba@gmail.com>
2019-06-06 21:03:19 -0400

    initial commit

---
+++ day1swift/main.swift
@@ -0,0 +1,35 @@
+//
+//  main.swift
+//  day1swift
+//
+//  Created by MacStudent on 2019-06-06.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+let 👍 = "hello"
+
+print(👍,π,你好,🐶🐮)
+
+print(Int.max,Int.min)
+
+var a = 10
+var b = 3
+
+var sum = a+b
+var sub = a-b
+var mul = a*b
+var div = a/b
+var mod = a%b
+print("addition is : \(sum)")
+print("subtraction is : \(sub)")
+print("multiplication is :,\(mul)")
+print("division is : \(div)")
+print("Mod is : \(mod)")
